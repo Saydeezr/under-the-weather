eb58ac07ccdbf41560880d8fab67e000a3a982af
Saydeezr <saydeezr@gmail.com>
2024-04-15 09:24:08 -0400

    cleared out comments and worked on the fetch

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
 
 const weatherApiKey = '223145bd460816c9dd292b66ab0ede9'
-//const weatherApiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`
-//const geoConvertApi =`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${geoConvertApi}`
-const input = document.getElementById('cityInput')
+// const input = document.getElementById('cityInput')
 
 
 submit.addEventListener('click',event => {
     event.preventDefault();
     getWeather();
-    // const location = location.value;
-    // if(location){
-    //     getData();
-    // } else 
-    // if(!location) {
-    //     alert('City name is required')
-    // }
+  
 })
 
 function getWeather () {
     const weatherApiKey = '223145bd460816c9dd292b66ab0ede9'
-    const city = document.getElementById('input').value;
+    const input = document.getElementById('cityInput').value;
 
-    if(!city) {
+    if(!input) {
         alert('Please enter a city')
         return;
     };
@@ -49,28 +41,49 @@ function getWeather () {
 }
 
 function displayWeather(data) {
-    const container = document.getElementById('weatherInfo')
-    let cityName = document.getElementById('cityName')
-    const icon = document.getElementById('weatherIcon')
-    const temperature = document.getElementById('temp')
-    const description = document.getElementById('description')
-    
-    container.innerHTML = '';
-    temperature.innerHTML = '';
-    description.innerHTML = '';
 
-    if(data.cod === '404') {
-        description.innerHTML = `<p>${data.message}<p>`;
-    } else{
-        let city = data.name;
-        let temp = Math.round(data.main.temp - 273.15);
-        let descript = data.weather[0].description;
-        let iconCode = data.weather[0].icon;
-        let iconURL = `https://openweathermap.org/img/wn/${iconCode}@4x.png`
-        
-        const temperature = `<p>${temp}°C</p>`;
-        const description = `<p>${city}</p> <p>${descript}</p>`;
-
-
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const container = document.getElementById('weatherInfo')
+    // let cityName = document.getElementById('cityName')
+    // const icon = document.getElementById('weatherIcon')
+    // const temperature = document.getElementById('temp')
+    // const description = document.getElementById('description')
+    
+    // container.innerHTML = '';
+    // temperature.innerHTML = '';
+    // description.innerHTML = '';
+
+    // if(data.cod === '404') {
+    //     description.innerHTML = `<p>${data.message}<p>`;
+    // } else{
+    //     let city = data.name;
+    //     let temp = Math.round(data.main.temp - 273.15);
+    //     let descript = data.weather[0].description;
+    //     let iconCode = data.weather[0].icon;
+    //     let iconURL = `https://openweathermap.org/img/wn/${iconCode}@4x.png`
+        
+    //     const temperature = `<p>${temp}°C</p>`;
+    //     const description = `<p>${city}</p> <p>${descript}</p>`;
+
+
+  
